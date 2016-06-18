5c77e577876347ed36977ea7df2a55d5ae4a9397
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-18 10:07:39 +0200

    Getters for base config values

--- src/Config.php
+++ src/Config.php
@@ -6,4 +6,43 @@ use Symfony\Component\HttpFoundation\ParameterBag;
 
 class Config extends ParameterBag
 {
+    /**
+     * @return array
+     */
+    public function getResponseGroup()
+    {
+        return $this->get('responsegroup');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->get('country');
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessKey()
+    {
+        return $this->get('accesskey');
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecretKey()
+    {
+        return $this->get('secretkey');
+    }
+
+    /**
+     * @return string
+     */
+    public function getAssociateTag()
+    {
+        return $this->get('associatetag');
+    }
 }
