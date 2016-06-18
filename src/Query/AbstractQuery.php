558a0dbdec35e5ce0c73cf6ba9de07360350c97a
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-18 14:59:48 +0200

    Move getApaiIO() into base

--- src/Query/AbstractQuery.php
+++ src/Query/AbstractQuery.php
@@ -2,8 +2,13 @@
 
 namespace Bolt\Extension\Bolt\AmazonApi\Query;
 
+use ApaiIO\ApaiIO;
+use ApaiIO\Configuration\GenericConfiguration;
+use ApaiIO\Request\GuzzleRequest;
 use Bolt\Extension\Bolt\AmazonApi\Config;
+use Bolt\Extension\Bolt\AmazonApi\Exception\InvalidConfigurationException;
 use Bolt\Extension\Bolt\AmazonApi\Records;
+use Bolt\Extension\Bolt\AmazonApi\ResponseTransformer\XmlToParameterBagObject;
 use GuzzleHttp\Client;
 
 /**
@@ -19,6 +24,9 @@ class AbstractQuery
     protected $records;
     /** @var Client */
     protected $guzzle;
+
+    /** @var \ApaiIO\ApaiIO */
+    private $apaiIO;
 
     /**
      * Constructor.
@@ -56,5 +64,50 @@ class AbstractQuery
     public function getGuzzle()
     {
         return $this->guzzle;
+    }
+
+    /**
+     * Get/setup ApaiIO object
+     *
+     * @throws InvalidConfigurationException
+     *
+     * @return \ApaiIO\ApaiIO
+     */
+    protected function getApaiIO()
+    {
+        if ($this->apaiIO) {
+            return $this->apaiIO;
+        }
+
+        if ($this->config->getCountry() === null) {
+            throw new InvalidConfigurationException("Missing Amazon API configuration parameter: 'country'");
+        }
+        if ($this->config->getAccessKey() === null) {
+            throw new InvalidConfigurationException("Missing Amazon API configuration parameter: 'accesskey'");
+        }
+        if ($this->config->getSecretKey() === null) {
+            throw new InvalidConfigurationException("Missing Amazon API configuration parameter: 'secretkey'");
+        }
+        if ($this->config->getAssociateTag() === null) {
+            throw new InvalidConfigurationException("Missing Amazon API configuration parameter: 'associatetag'");
+        }
+
+        // Initialise customer configuration
+        $conf = new GenericConfiguration();
+
+        // Set up the PHP API object
+        $conf
+            ->setCountry($this->config->getCountry())
+            ->setAccessKey($this->config->getAccessKey())
+            ->setSecretKey($this->config->getSecretKey())
+            ->setAssociateTag($this->config->getAssociateTag())
+            ->setRequest(new GuzzleRequest($this->getGuzzle()))
+            ->setResponseTransformer(new XmlToParameterBagObject())
+        ;
+
+        // Initialise the API with configuration
+        $this->apaiIO = new ApaiIO($conf);
+
+        return $this->apaiIO;
     }
 }
