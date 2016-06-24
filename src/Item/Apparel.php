1ebbe11b4c77636029bcb65f2860023c56da4726
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-24 12:26:26 +0200

    Remove unused constructor overrides

--- src/Item/Apparel.php
+++ src/Item/Apparel.php
@@ -4,11 +4,4 @@ namespace Bolt\Extension\Bolt\AmazonApi\Item;
 
 class Apparel extends AbstractItem
 {
-    /**
-     * Constructor.
-     */
-    public function __construct(array $data)
-    {
-        parent::__construct($data);
-    }
 }
--- src/Item/GenericItem.php
+++ src/Item/GenericItem.php
@@ -4,11 +4,4 @@ namespace Bolt\Extension\Bolt\AmazonApi\Item;
 
 class GenericItem extends AbstractItem
 {
-    /**
-     * Constructor.
-     */
-    public function __construct(array $data)
-    {
-        parent::__construct($data);
-    }
 }
